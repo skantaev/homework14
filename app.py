6d5eb562804fbfffd57cd6253fd7aecf31522252
skantaev <s.kantaev@gmail.com>
2018-03-20 16:34:47 +0300

    final version with HTML and CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, flash, render_template
+from flask import Flask, request, flash, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 import config
@@ -9,11 +9,19 @@ app.config.from_object(config)
 db = SQLAlchemy(app)
 
 
-@app.route('/posts', methods=['GET', 'POST'])
+# Домашняя страница переводит сразу на гостевую книгу
+@app.route('/')
+def home():
+    return redirect(url_for('index'))
+
+
+# Посты гостевой книги
+@app.route('/posts/', methods=['GET', 'POST'])
 def index():
     from models import Post
     from forms import PostForm
 
+    # Добавление поста
     if request.method == 'POST':
         form = PostForm(request.form)
 
@@ -26,23 +34,26 @@ def index():
 
         else:
             flash('Форма не валидна! Пост не был опубликован.')
-            flash(str(form.errors))
 
     posts = Post.query.all()
 
-    return render_template('posts.txt', posts=posts)
+    # Вывод всех постов
+    return render_template('show_posts.html', posts=posts)
 
 
-@app.route('/posts/<int:post_id>', methods=['GET', 'POST'])
+# Маршрут для конкретного поста по его id
+@app.route('/posts/<int:post_id>/', methods=['GET', 'POST'])
 def show_post(post_id):
     from models import Comment, Post
     from forms import CommentForm
 
+    # Получение определнного поста из базы данных по id из URL
     post = Post.query.filter_by(id=post_id).first()
 
     if post is None:
         return 'Not Found', 404
 
+    # Добавление комментария
     if request.method == 'POST':
         form = CommentForm(request.form)
 
@@ -50,6 +61,7 @@ def show_post(post_id):
             comment = Comment(post=post, content=request.form['content'])
             db.session.add(comment)
 
+            # Для поста увеличиваем переменную, хранящую количество комментариев
             post.number_of_comments += 1
 
             db.session.commit()
@@ -58,11 +70,12 @@ def show_post(post_id):
 
         else:
             flash('Форма не валидна! Комментарий не был опубликован.')
-            flash(str(form.errors))
 
+    # Получение всех комментариев из базы данных к текущему посту
     comments = Comment.query.filter_by(post_id=post.id).all()
 
-    return render_template('item.txt', post=post, comments=comments)
+    # Вывод поста и комментариев к нему
+    return render_template('show_comments.html', post=post, comments=comments)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,9 +1,5 @@
 from wtforms_alchemy import ModelForm
-from wtforms.fields import StringField
-from wtforms.validators import Length
 from models import Post, Comment
-
-content_validator = StringField(validators=[Length(min=6, max=3000)])
 
 
 class PostForm(ModelForm):
@@ -11,13 +7,8 @@ class PostForm(ModelForm):
         model = Post
         only = ['title', 'content']
 
-    title = StringField(validators=[Length(min=3, max=60)])
-    content = content_validator
-
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         only = ['content']
-
-    content = content_validator
